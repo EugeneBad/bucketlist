927572af7f3e00f431996e6090e399c8d4e96708
eugenius <eugene.badriku@andela.com>
2017-07-21 13:33:24 +0300

    Add rest of views and tests

--- app/api/views.py
+++ app/api/views.py
@@ -8,7 +8,7 @@ from flask_restful.reqparse import RequestParser
 from app.db.models import Bucketlist, Item, User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.exc import IntegrityError
 from app.app import app
 
 db_engine = create_engine(app.config.get('SQLALCHEMY_DATABASE_URI'))
@@ -125,3 +125,272 @@ class Login(Request, Resource):
         auth_token = jwt.encode(payload, key=SECRET_KEY).decode()
 
         return {'auth_token': auth_token}, 200
+
+
+class Bucketlists(Request, Resource):
+    """
+    Class based view that handles: display of bucketlists using the GET http verb
+    and creation of bucketlists using the POST http verb
+    """
+    def get(self):
+        """ Called for a GET request """
+        # View is only accessible by a logged in user.
+        if not self.is_authenticated():
+            return 'Please login', 401
+
+        bucketlists = Bucketlist.query.filter_by(created_by=self.current_user)
+
+        if not len(list(bucketlists)):
+            return {'Bucketlists': 'None'}, 200
+
+        paginated_list = self.paginated(bucketlists)
+
+        # List comprehension that generates individual dictionaries of bucketlists
+        list_of_bucketlists = [{'id': bucketlist.id,
+                                'name': bucketlist.name,
+                                'items': 'None' if not bucketlist.items else len(bucketlist.items),
+                                'date_created': str(bucketlist.creation_date),
+                                'created_by': self.current_user.username} for bucketlist in paginated_list]
+        return {'Bucketlists': list_of_bucketlists,
+                'Page': '{} of {}'.format(self.page, self.total_pages)}, 200
+
+    def post(self):
+        """ Called for a POST request """
+        if not self.is_authenticated():
+            return 'Please login', 401
+
+        request_args = self.parse_args()
+
+        if not request_args.get('name'):
+            return 'Bucketlist name needed', 400
+
+        try:
+            new_bucketlist = Bucketlist(name=request_args.get('name'),
+                                        created_by=self.current_user)
+            session.add(new_bucketlist)
+            session.commit()
+            return 'Bucketlist successfully created', 200
+
+        # Raised by Database API when the unique constraint on name is violated
+        except IntegrityError:
+            session.rollback()
+            return 'Bucketlist name already exists', 409
+
+
+class BucketlistDetail(Request, Resource):
+    """
+        Class based view that handles: display of a bucketlist's details using the GET http verb
+        and updating of a bucketlist using the PUT http verb, as well as deleting
+        using the DELETE http verb.
+    """
+
+    def get(self, bucketlist_id):
+        """ Called for a GET request """
+        if not self.is_authenticated():
+            return 'Please login', 401
+
+        bucketlist = Bucketlist.query.filter_by(id=bucketlist_id,
+                                                created_by=self.current_user).first()
+        if not bucketlist:
+            return 'Bucketlist does not exist', 404
+
+        if bucketlist.items:
+            bucketlist_items = [{'id': item.id,
+                                 'name': item.name,
+                                 'done': item.completed} for item in bucketlist.items]
+        else:
+            bucketlist_items = 'None'
+
+        bucketlist_detail = {'id': bucketlist.id,
+                             'name': bucketlist.name,
+                             'items': bucketlist_items,
+                             'created_by': bucketlist.created_by.username,
+                             'date_created': str(bucketlist.creation_date),
+                             'date_modified': str(bucketlist.modification_date)}
+
+        return bucketlist_detail, 200
+
+    def put(self, bucketlist_id):
+        """ Called for a PUT request """
+        if not self.is_authenticated():
+            return 'Please login', 401
+
+        request_args = self.parse_args()
+        bucketlist = session.query(Bucketlist).filter_by(id=bucketlist_id,
+                                                         created_by=self.current_user).first()
+
+        if bucketlist and request_args.get('name'):
+            try:
+                # Update bucketlist name and commit to the database
+                bucketlist.name = request_args.get('name')
+                session.add(bucketlist)
+                session.commit()
+                return 'Bucketlist successfully updated', 200
+
+            except IntegrityError:
+                session.rollback()
+                return 'Bucketlist name already exists', 409
+
+        elif bucketlist and not request_args.get('name'):
+            return 'Supply new bucketlist name', 400
+
+        else:
+            return 'Bucketlist does not exist', 404
+
+    def delete(self, bucketlist_id):
+        """ Called for a DELETE request """
+        if not self.is_authenticated():
+            return 'Please login', 401
+        bucketlist = session.query(Bucketlist).filter_by(id=bucketlist_id,
+                                                         created_by=self.current_user).first()
+
+        if not bucketlist:
+            return 'Bucketlist does not exist', 404
+
+        session.delete(bucketlist)
+        session.commit()
+        return 'Bucketlist successfully deleted', 200
+
+
+class BucketlistItems(Request, Resource):
+    """
+    View class used to display items in a particular bucketlist using the GET http verb;
+     as well as create new items using the POST http verb.
+    """
+
+    def get(self, bucketlist_id):
+        """ Called with the GET http verb """
+        # Only authenticated users
+        if not self.is_authenticated():
+            return 'Please login', 401
+        bucketlist = Bucketlist.query.filter_by(id=bucketlist_id,
+                                                created_by=self.current_user).first()
+
+        if not bucketlist:
+            return 'Bucketlist does not exist', 404
+
+        bucketlist_items = Item.query.filter_by(bucketlist=bucketlist)
+
+        # For a bucketlist with no items
+        if not len(list(bucketlist_items)):
+            return {'Items': 'None'}, 200
+
+        bucketlist_items = [{'id': item.id,
+                             'name': item.name,
+                             'done': item.completed} for item in self.paginated(bucketlist_items)]
+        return {'Items': bucketlist_items,
+                'Page': '{} of {}'.format(self.page, self.total_pages)}, 200
+
+    def post(self, bucketlist_id):
+        """ Called with the POST http verb """
+        # Only authenticated users
+        if not self.is_authenticated():
+            return 'Please login', 401
+        request_args = self.parse_args()
+        bucketlist = session.query(Bucketlist).filter_by(id=bucketlist_id,
+                                                         created_by=self.current_user).first()
+
+        if not bucketlist:
+            return 'Bucketlist does not exist', 404
+
+        if not request_args.get('name'):
+            return 'Item name not supplied', 400
+
+        try:
+            new_item = Item(name=request_args.get('name'), bucketlist=bucketlist)
+
+            session.add(new_item)
+            session.commit()
+            return 'New item added successfully', 200
+
+        except IntegrityError:
+            session.rollback()
+            return 'Item name already exists', 409
+
+
+class BucketListItemDetail(Request, Resource):
+    """
+        View class used to display an item's details using the GET http verb;
+         as well as update items using the PUT http verb.
+         Also deletes items using the DELETE http verb
+    """
+    def get(self, bucketlist_id, item_id):
+        """ Called with a GET request """
+        # Only authenticated users
+        if not self.is_authenticated():
+            return 'Please login', 401
+        bucketlist = session.query(Bucketlist).filter_by(id=bucketlist_id,
+                                                         created_by=self.current_user).first()
+
+        if not bucketlist:
+            return 'Bucketlist does not exist', 404
+
+        item = Item.query.filter_by(id=item_id, bucketlist=bucketlist).first()
+        if not item:
+            return 'Item does not exist', 404
+
+        item_detail = {'id': item.id,
+                       'name': item.name,
+                       'done': item.completed,
+                       'creation_date': str(item.creation_date),
+                       'last_modified': str(item.modification_date)}
+        return item_detail, 200
+
+    def put(self, bucketlist_id, item_id):
+        """ Called with a PUT request """
+        # Only authenticated users
+        if not self.is_authenticated():
+            return 'Please login', 401
+
+        request_args = self.parse_args()
+
+        # Missing item name
+        if not request_args.get('name'):
+            return 'Item name needed', 400
+
+        bucketlist = session.query(Bucketlist).filter_by(id=bucketlist_id,
+                                                         created_by=self.current_user).first()
+
+        if not bucketlist:
+            return 'Bucketlist does not exist', 404
+
+        item = session.query(Item).filter_by(id=item_id,
+                                             bucketlist=bucketlist).first()
+
+        if not item:
+            return 'Item does not exist', 404
+
+        try:
+            if request_args.get('name'):
+                item.name = request_args.get('name')
+
+            if request_args.get('done'):
+                item.completed = request_args.get('done')
+
+            session.add(item)
+            session.commit()
+            return 'Item updated', 200
+
+        except IntegrityError:
+            return 'Item name already exists', 409
+
+    def delete(self, bucketlist_id, item_id):
+        """ Called with a DELETE request """
+        # Only authenticated users
+        if not self.is_authenticated():
+            return 'Please login', 401
+        bucketlist = session.query(Bucketlist).filter_by(id=bucketlist_id,
+                                                         created_by=self.current_user).first()
+
+        if not bucketlist:
+            return 'Bucketlist does not exist', 404
+
+        item = session.query(Item).filter_by(id=item_id,
+                                             bucketlist=bucketlist).first()
+        if not item:
+            return 'Item does not exist', 404
+
+        session.delete(item)
+        session.commit()
+        return 'Item deleted', 200
+
--- tests/test_bucketlists.py
+++ tests/test_bucketlists.py
@@ -1,27 +1,173 @@
-from flask_restful.reqparse import RequestParser
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from app.app import app
-
-db_engine = create_engine(app.config.get('SQLALCHEMY_DATABASE_URI'))
-session = sessionmaker(bind=db_engine)()
-SECRET_KEY = app.config.get('SECRET_KEY')
+from itertools import cycle
+from . test_base import BaseTest
+import json
 
 
-class Request(RequestParser):
-    def __init__(self):
-        """
-        Utility class used for performing generic manipulations on incoming request objects.
-        Initialised with all the arguments to be used by the view classes.
+class BucketlistsTest(BaseTest):
 
-        """
-        super().__init__()
-        self.add_argument('name', location='form')
-        self.add_argument('done', location='form')
-        self.add_argument('username', location='form')
-        self.add_argument('password', location='form')
-        self.add_argument('token', location='headers')
-        self.add_argument('page', location='args')
-        self.add_argument('limit', location='args')
+    def setUp(self):
+        super().setUp()
+        for name in ['Food', 'Travel', 'People', 'Movies', 'Concerts']:
+            self.app.post('api/V1/bucketlists',
+                          headers={'token': self.auth_token},
+                          data={'name': name})
+
+    # Authentication required
+    def test_authentication_requirement(self):
+        get_response = self.app.get('api/V1/bucketlists')
+        post_response = self.app.post('api/V1/bucketlists')
+        self.assertTrue(get_response.status_code == 401,
+                        msg='Bucketlists view accepts unauthenticated GET requests')
+        self.assertTrue(post_response.status_code == 401,
+                        msg='Bucketlists view accepts unauthenticated POST requests')
+
+        get_response = self.app.get('api/V1/bucketlists',
+                                    headers={'token': self.auth_token})
+
+        self.assertEqual(get_response.status_code, 200,
+                         msg='Bucketlists view rejects authenticated users')
+
+    # Response if no bucketlist
+    def test_response_before_adding_bucketlist(self):
+        response = self.app.get('api/V1/bucketlists',
+                                headers={'token': self.auth_token})
+        self.assertEqual(response.status_code, 200)
+
+    # Create with missing name
+    def test_fail_create_bucketlist_with_missing_name(self):
+        post_response = self.app.post('api/V1/bucketlists',
+                                      headers={'token': self.auth_token},
+                                      data={'name': ''})
+        self.assertEqual(post_response.status_code, 400,
+                         msg='Bucketlists view accepts creation with missing name')
+
+    # Create with valid name
+    def test_create_bucketlist_with_valid_name(self):
+        post_response = self.app.post('api/V1/bucketlists',
+                                      headers={'token': self.auth_token},
+                                      data={'name': 'Cars'})
+
+        self.assertEqual(post_response.status_code, 200,
+                         msg='Bucketlists view does not create new bucketlist')
+
+    # Create with duplicate name
+    def test_fail_create_bucketlist_duplicate_name(self):
+
+        duplicate_name_response = self.app.post('api/V1/bucketlists',
+                                                headers={'token': self.auth_token},
+                                                data={'name': 'Travel'})
+
+        self.assertEqual(duplicate_name_response.status_code, 409,
+                         msg='Bucketlists view accepts a duplicate bucketlist name')
+
+    # Response if bucketlists exist
+    def test_response_on_existing_bucketlists(self):
+
+        response = self.app.get('api/V1/bucketlists',
+                                headers={'token': self.auth_token})
+
+        self.assertEqual(response.status_code, 200,
+                         msg='Bucketlists view not successfully fetching bucketlists')
+
+        response_content = json.loads(response.data.decode())
+        self.assertEqual(len(response_content.get('Bucketlists')), 5,
+                         msg='Bucketlists view returns wrong number of bucketlists')
+
+        expected_names = cycle(['Food', 'Travel', 'People', 'Movies', 'Concerts'])
+
+        for bucketlist in response_content.get('Bucketlists'):
+            self.assertEqual(bucketlist.get('name'), next(expected_names),
+                             msg='Correct names of bucketlists not returned')
+
+    def test_pagination(self):
+        response = self.app.get('api/V1/bucketlists?limit=3&page=1',
+                                headers={'token': self.auth_token})
+
+        response_content = json.loads(response.data.decode())
+        self.assertEqual(len(response_content.get('Bucketlists')), 3,
+                         msg='Bucketlists view returns wrong number of bucketlists per page')
+
+        response = self.app.get('api/V1/bucketlists?limit=2&page=2',
+                                headers={'token': self.auth_token})
+
+        response_content = json.loads(response.data.decode())
+        self.assertEqual(len(response_content.get('Bucketlists')), 2,
+                         msg='Bucketlists view returns wrong number of bucketlists per page')
+
+
+class TestBucketlistDetail(BaseTest):
+
+    def setUp(self):
+        super().setUp()
+        for name in ['Food', 'Travel', 'People', 'Movies', 'Concerts']:
+            self.app.post('api/V1/bucketlists',
+                          headers={'token': self.auth_token},
+                          data={'name': name})
+
+    # Authentication requirement
+    def test_authentication_requirement(self):
+        get_response = self.app.get('api/V1/bucketlists/1')
+        put_response = self.app.get('api/V1/bucketlists/1')
+        delete_response = self.app.delete('api/V1/bucketlists/1')
+
+        self.assertEqual(get_response.status_code, 401,
+                        msg='BucketlistDetail view accepts unauthenticated GET requests')
+        self.assertEqual(put_response.status_code, 401,
+                        msg='BucketlistDetail view accepts unauthenticated PUT requests')
+        self.assertEqual(delete_response.status_code, 401,
+                        msg='BucketlistDetail view accepts unauthenticated DELETE requests')
+
+    # Non existent bucketlist
+    def test_get_response_non_existent_bucketlist(self):
+        get_response = self.app.get('api/V1/bucketlists/94759485',
+                                    headers={'token': self.auth_token})
+
+        self.assertEqual(get_response.status_code, 404,
+                         msg='404 not returned for non-existent bucketlists')
+
+    # Getting existent bucketlist
+    def test_get_response_when_bucketlist_existent(self):
+
+        get_response = self.app.get('api/V1/bucketlists/1',
+                                    headers={'token': self.auth_token})
+
+        self.assertEqual(get_response.status_code, 200,
+                         msg='BucketlistDetail does not return details of existent bucketlist')
+
+    # Updating with no name
+    def test_fail_when_update_name_empty(self):
+        put_response = self.app.put('api/V1/bucketlists/1',
+                                    headers={'token': self.auth_token}, data={'name': ''})
+
+        self.assertEqual(put_response.status_code, 400,
+                         msg='BucketlistDetail view does not reject updating with an empty name')
+
+    # Updating with duplicate name
+    def test_fail_when_update_name_duplicate(self):
+        put_response = self.app.put('api/V1/bucketlists/1',
+                                    headers={'token': self.auth_token},
+                                    data={'name': 'Travel'})
+
+        self.assertEqual(put_response.status_code, 409,
+                         msg='BucketlistDetail view does not reject updating with a duplicate name')
+
+    # Updating with valid name
+    def test_update_with_new_bucketlist_name(self):
+        put_response = self.app.put('api/V1/bucketlists/1',
+                                    headers={'token': self.auth_token},
+                                    data={'name': 'Cars'})
+
+        self.assertEqual(put_response.status_code, 200,
+                         msg='BucketlistDetail view does not update bucketlist name')
+
+    # Successful deletion
+    def test_deletion(self):
+        delete_response = self.app.delete('api/V1/bucketlists/1',
+                                    headers={'token': self.auth_token})
+
+        self.assertEqual(delete_response.status_code, 200, msg='200 not returned for successful deletion')
+        get_response = self.app.get('api/V1/bucketlists/1',
+                                    headers={'token': self.auth_token})
+        self.assertEqual(get_response.status_code, 404,
+                         msg='DELETE request to BucketlistDetail does not delete bucketlist')
+
