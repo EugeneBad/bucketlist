71ac9b26d1b61b792691689a795808a2e35aa586
eugenius <eugene.badriku@andela.com>
2017-07-21 11:24:14 +0300

    Add items table to models.py

--- app/db/models.py
+++ app/db/models.py
@@ -18,3 +18,15 @@ class Bucketlist(db.Model):
     items = db.relationship('Item', backref='bucketlist')
 
 
+class Item(db.Model):
+    __tablename__ = 'item'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120),
+                     unique=True,
+                     nullable=False)
+    creation_date = db.Column(db.Date, default=datetime.now().date())
+    modification_date = db.Column(db.Date,
+                                  default=datetime.now().date(),
+                                  onupdate=datetime.now().date())
+    completed = db.Column(db.Boolean, default=False)
+    bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlist.id'))
