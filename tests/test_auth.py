f7b163718fe7a73fc88059209110d91bd60d9146
eugenius <eugene.badriku@andela.com>
2017-07-21 12:40:45 +0300

    Add test for missing username

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,3 +7,29 @@ class TestRegisterView(BaseTest):
     def test_not_post_method(self):
         response = self.app.get('api/V1/auth/register')
         self.assertEqual(response.status_code, 405)
+
+    # Missing arguments
+    def test_missing_username_password(self):
+        response = self.app.post('api/V1/auth/register')
+        self.assertEqual(response.status_code, 400,
+                         msg='Register view accepts missing username and password')
+
+        response = self.app.post('api/V1/auth/register',
+                                 data={'other_arg_1': 'test', 'other_arg_2': 'test'})
+        self.assertEqual(response.status_code, 400,
+                         msg='Register view considers any arguments as username and password')
+
+        response = self.app.post('api/V1/auth/register',
+                                 data={'username': '', 'password': ''})
+        self.assertEqual(response.status_code, 400,
+                         msg='Register view accepts empty username and password')
+
+        response = self.app.post('api/V1/auth/register',
+                                 data={'username': 'master', 'password': ''})
+        self.assertEqual(response.status_code, 400,
+                         msg='Register view accepts username without password')
+
+        response = self.app.post('api/V1/auth/register',
+                                 data={'username': '', 'password': 'master'})
+        self.assertEqual(response.status_code, 400,
+                         msg='Register view accepts password without username')
