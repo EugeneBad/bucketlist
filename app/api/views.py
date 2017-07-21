db2cd8f3f67214f80f4fc65014a9af556b9e53fb
eugenius <eugene.badriku@andela.com>
2017-07-21 11:52:29 +0300

    Add view classes to views.py

---
+++ app/api/views.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource
+
+class Login(Resource):
+    pass
+
+
+class Bucketlists(Resource):
+    pass
+
+class BucketlistDetail(Resource):
+    pass
+
+class BucketlistItems(Resource):
+    pass
+
+class BucketListItemDetail(Resource):
+    pass
