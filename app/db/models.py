af3c858605a2d7eb1fdcea80f444baf1314737f5
eugenius <eugene.badriku@andela.com>
2017-07-21 11:25:32 +0300

    Add a users table to the models.py

--- app/db/models.py
+++ app/db/models.py
@@ -30,3 +30,10 @@ class Item(db.Model):
                                   onupdate=datetime.now().date())
     completed = db.Column(db.Boolean, default=False)
     bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlist.id'))
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(300), nullable=False)
