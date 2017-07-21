d375f77a4e06a21aca4e216a999d9d93cebd8a8c
eugenius <eugene.badriku@andela.com>
2017-07-21 11:32:43 +0300

    Add creation of Api object

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
 import os
-
 from flask import Flask
-
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config.from_object(os.getenv('APP_SETTINGS'))
 
-
+api = Api(app)
+db = SQLAlchemy(app)
+db.create_all()
