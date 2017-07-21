433699bca9797c34fc573325574ddd51bc23e263
eugenius <eugene.badriku@andela.com>
2017-07-21 12:20:59 +0300

    Add base test user

--- tests/test_base.py
+++ tests/test_base.py
@@ -7,7 +7,10 @@ class BaseTest(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         db.create_all()
-        
+        register_user = self.app.post('api/V1/auth/register',
+                                      data={'username': 'admin', 'password': 'admin'})
+        self.auth_token = json.loads(register_user.data.decode()).get('auth_token')
+
     def tearDown(self):
         db.drop_all()
 
