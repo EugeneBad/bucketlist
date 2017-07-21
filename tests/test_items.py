62da5f4c62b111d4e875102b90b8649941cc7417
eugenius <eugene.badriku@andela.com>
2017-07-21 14:56:26 +0300

    Add authentication requirement test for BucketlistItem view

--- tests/test_items.py
+++ tests/test_items.py
@@ -13,3 +13,18 @@ class TestBucketlistItems(BaseTest):
             self.app.post('api/V1/bucketlists/1/items',
                           headers={'token': self.auth_token},
                           data={'name': city})
+
+    # Authentication required
+    def test_authentication_requirement(self):
+        get_response = self.app.get('api/V1/bucketlists/1/items')
+        post_response = self.app.post('api/V1/bucketlists/1/items')
+        self.assertEqual(get_response.status_code, 401,
+                         msg='BucketlistItems view accepts unauthenticated GET requests')
+        self.assertEqual(post_response.status_code, 401,
+                         msg='BucketlistItems view accepts unauthenticated POST requests')
+
+        get_response = self.app.get('api/V1/bucketlists/1/items',
+                                    headers={'token': self.auth_token})
+
+        self.assertEqual(get_response.status_code, 200,
+                         msg='BucketlistItems view rejects authenticated users')
