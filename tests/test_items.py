a0cf87bffb094d46968160fc3f3c36e0cf202fb0
eugenius <eugene.badriku@andela.com>
2017-07-21 15:16:43 +0300

    Add test for duplicate item name creation

--- tests/test_items.py
+++ tests/test_items.py
@@ -38,12 +38,19 @@ class TestBucketlistItems(BaseTest):
                          msg='BucketlistItems view does not return 400 for POST with missing name')
 
     # Create with valid name
-    def test_create_bucketlist_with_valid_name(self):
+    def test_create_item_with_valid_name(self):
         post_response = self.app.post('api/V1/bucketlists/1/items',
                                       headers={'token': self.auth_token},
                                       data={'name': 'Beijing'})
 
         self.assertEqual(post_response.status_code, 200,
-                         msg='Bucketlists view does not create new bucketlist')
+                         msg='BucketlistItems view does not create new bucketlist')
 
-    
+    # Create with duplicate name
+    def test_fail_create_item_duplicate_name(self):
+        duplicate_name_response = self.app.post('api/V1/bucketlists/1/items',
+                                                headers={'token': self.auth_token},
+                                                data={'name': 'Venice'})
+
+        self.assertEqual(duplicate_name_response.status_code, 409,
+                         msg='BucketlistItems view accepts a duplicate bucketlist name')
